7b86fdab12a2382d1d9b0bc09ae5f5356a2bfcc4
KaterinaShpilevskaya <katyashpilevskaya@gmail.com>
2022-08-09 12:23:12 +0300

    init:added todo

--- app/getDate.js
+++ app/getDate.js
@@ -4,6 +4,8 @@ export const getDate = (date) => {
     let year = date.getFullYear();
     let hours = date.getHours();
     let minutes = date.getMinutes();
+    month < 10 ? (month = '0' + month) : month;
+    newdate < 10 ? (newdate = '0' + newdate) : newdate;
     minutes < 10 ? (minutes = '0' + minutes) : minutes;
     return `${newdate}-${month}-${year} | ${hours}:${minutes}`;
   };
